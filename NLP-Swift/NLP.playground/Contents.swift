b23392a54ddf85d3f61c0645be750c49356d1b72
karthik <karthik.v17@outlook.com>
2018-07-11 22:26:14 +0530

    NLP Swift
    NSLinguisticTagScheme are practised and view output of NSLinguisticTagger enumeration

--- NLP-Swift/NLP.playground/Contents.swift
+++ NLP-Swift/NLP.playground/Contents.swift
@@ -4,13 +4,13 @@ import Foundation
 // https://medium.com/swiftworld/swift-world-whats-new-in-ios-11-natural-language-processing-2a16b7422334
 
 debugPrint("*****************************************")
+debugPrint("***************Tokenization**************")
 debugPrint("*****************************************")
-debugPrint("*****************************************")
-// Enumerating string
+// Tokenization
 var string : String = "The American Red Cross was established in Washington, D.C., by Clara Barton."
 var range = NSRange(location:0, length: string.utf16.count)
 var tagger : NSLinguisticTagger = NSLinguisticTagger.init(tagSchemes: [.tokenType], options: 0)
-var options : NSLinguisticTagger.Options = [.omitPunctuation]
+var options : NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace]
 tagger.string = string
 tagger.enumerateTags(in: range, unit: .word, scheme: .tokenType, options: options) { (tag, tokenRange, stop) in
     if let tag = tag {
@@ -20,25 +20,25 @@ tagger.enumerateTags(in: range, unit: .word, scheme: .tokenType, options: option
 }
 
 debugPrint("*****************************************")
-debugPrint("*****************************************")
+debugPrint("**********Language Identification********")
 debugPrint("*****************************************")
 // Language Identification
 tagger = NSLinguisticTagger(tagSchemes: [.language], options: 0)
 string = "WWDC 2017 已经结束了。"
 tagger.string = string
 if let language = tagger.dominantLanguage {
-    print(language)
+    print("Language: ", language)
 } else {
     print("can't get domainant language")
 }
 
 debugPrint("*****************************************")
+debugPrint("**************Tokenization***************")
 debugPrint("*****************************************")
-debugPrint("*****************************************")
-//Tokenization
+// Tokenization
 string = "The American Red Cross was established in Washington, D.C., by Clara Barton."
 range = NSRange(location:0, length: string.utf16.count)
-tagger = NSLinguisticTagger.init(tagSchemes: [.tokenType, .nameType], options: 0)
+tagger = NSLinguisticTagger.init(tagSchemes: [.nameType], options: 0)
 tagger.string = string
 options = [.omitPunctuation, .omitWhitespace, .joinNames]
 let tags: [NSLinguisticTag] = [.personalName, .placeName, .organizationName]
@@ -49,3 +49,51 @@ tagger.enumerateTags(in: range, unit: .word, scheme: .nameType, options: options
     }
 }
 
+debugPrint("*****************************************")
+debugPrint("**************Lemmatization**************")
+debugPrint("*****************************************")
+// Lemmatization
+string = "The American Red Cross was established in Washington, D.C., by Clara Barton."
+range = NSRange(location:0, length: string.utf16.count)
+tagger = NSLinguisticTagger.init(tagSchemes: [.lemma], options: 0)
+tagger.string = string
+options = [.omitPunctuation, .omitWhitespace]
+tagger.enumerateTags(in: range, unit: .word, scheme: .lemma, options: options) { tag, tokenRange, stop in
+    if let lemma = tag?.rawValue {
+        print(lemma)
+    }
+}
+
+debugPrint("*****************************************")
+debugPrint("*************Lexical Classes*************")
+debugPrint("*****************************************")
+// Lexical Classes
+string = "The American Red Cross was established in Washington, D.C., by Clara Barton."
+range = NSRange(location:0, length: string.utf16.count)
+tagger = NSLinguisticTagger.init(tagSchemes: [.lexicalClass], options: 0)
+tagger.string = string
+options = [.omitPunctuation, .omitWhitespace]
+tagger.enumerateTags(in: range, unit: .word, scheme: .lexicalClass, options: options) { tag, tokenRange, stop in
+    if let lemma = tag?.rawValue {
+        print(lemma)
+    }
+}
+
+debugPrint("*****************************************")
+debugPrint("******************Script*****************")
+debugPrint("*****************************************")
+// Script Type
+string = "The American Red Cross was established in Washington, D.C., by Clara Barton."
+range = NSRange(location:0, length: string.utf16.count)
+tagger = NSLinguisticTagger.init(tagSchemes: [.script], options: 0)
+tagger.string = string
+options = [.omitPunctuation, .omitWhitespace]
+tagger.enumerateTags(in: range, unit: .word, scheme: .script, options: options) { tag, tokenRange, stop in
+    if let lemma = tag?.rawValue {
+        print(lemma)
+    }
+}
+
+debugPrint("*****************************************")
+debugPrint("*****************************************")
+debugPrint("*****************************************")
